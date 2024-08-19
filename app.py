65b4f19a55568389617bf2a4aa874f7d61ca35ff
이수빈 <kl8501@naver.com>
2024-08-19 10:08:08 +0900

    커밋 메시지

--- app.py
+++ app.py
@@ -1,22 +1,33 @@
 import pandas as pd
 import numpy as np
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # 피클 파일을 로드 (음료 데이터)
 df = pd.read_pickle('data/cafe.pkl')
 
+# 카페 목록 및 음료 리스트 제공 API
+@app.route('/api/menus/<string:cafe_name>', methods=['GET'])
+def get_menus(cafe_name):
+    # 카페명으로 필터링
+    filtered_df = df[df['카페명'] == cafe_name]
+    menu_list = filtered_df[['카페명', '음료명']].to_dict(orient='records')
+    
+    return jsonify(menu_list)
+
+
 # 음료 추천을 처리하는 API
 @app.route('/api/recommend', methods=['POST'])
 def recommend():
-    # 프론트엔드에서 전송된 JSON 데이터를 받음
     data = request.json
-    selected_cafes = data.get('cafes', [])
-    caffeine = data.get('caffeine', 1)  # 기본값: 카페인 있음
-    coffee = data.get('coffee', 1)  # 기본값: 커피 있음
-    max_calories = data.get('calories', 942)  # 기본 최대 칼로리
-    max_sugar = data.get('sugar', 217)  # 기본 최대 당류
+    selected_cafes = data['cafes']
+    caffeine = data['caffeine']
+    coffee = data['coffee']
+    max_calories = data['calories']
+    max_sugar = data['sugar']
 
     # 선택한 카페 목록과 조건에 맞는 데이터 필터링
     filtered_df = df[(df['카페명'].isin(selected_cafes)) & 
@@ -27,11 +38,35 @@ def recommend():
     filtered_df['score'] = np.sqrt((filtered_df['칼로리'] - max_calories)**2 + 
                                    (filtered_df['당류'] - max_sugar)**2)
     
-    # 가장 가까운 5개 음료 추천
-    recommendations = filtered_df.nsmallest(5, 'score')
+    # 가장 가까운 10개 음료 추천
+    recommendations = filtered_df.nsmallest(10, 'score')
+    recommendations_list = recommendations.to_dict(orient='records')
 
-    # JSON 형식으로 응답 반환
-    return jsonify(recommendations.to_dict(orient='records'))
+    # 1~5등 반환
+    return jsonify(recommendations_list[:5])
+
+# 재추천 API
+@app.route('/api/recommend/more', methods=['POST'])
+def recommend_more():
+    data = request.json
+    selected_cafes = data['cafes']
+    caffeine = data['caffeine']
+    coffee = data['coffee']
+    max_calories = data['calories']
+    max_sugar = data['sugar']
+
+    filtered_df = df[(df['카페명'].isin(selected_cafes)) & 
+                     (df['카페인유무'] == caffeine) & 
+                     (df['커피유무'] == coffee)]
+    
+    filtered_df['score'] = np.sqrt((filtered_df['칼로리'] - max_calories)**2 + 
+                                   (filtered_df['당류'] - max_sugar)**2)
+    
+    recommendations = filtered_df.nsmallest(10, 'score')
+    recommendations_list = recommendations.to_dict(orient='records')
+
+    # 6~10등 반환
+    return jsonify(recommendations_list[5:10])
 
 if __name__ == '__main__':
     app.run(debug=True)
