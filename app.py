b9cd484030f406debe098382a84e2a7a35da34b7
soobin0910 <kl8501@naver.com>
2024-08-21 14:03:25 +0900

    Update app.py
    예외 처리 추가
    (조건에 맞는 음료가 0개일 경우)

--- app.py
+++ app.py
@@ -34,6 +34,10 @@ def recommend():
                      (df['카페인유무'] == caffeine) & 
                      (df['커피유무'] != coffee)]
     
+    # 예외 처리: 조건에 맞는 음료가 0개일 경우
+    if len(filtered_df) == 0:
+        return jsonify({"error": "조건에 맞는 음료가 없습니다."}), 404
+        
     # 칼로리와 당류 특성만 추출하여 스케일링 (표준화)
     scaler = StandardScaler()
     scaled_features = scaler.fit_transform(filtered_df[['칼로리', '당류']])
@@ -70,6 +74,10 @@ def recommend_more():
     filtered_df = df[(df['카페명'].isin(selected_cafes)) & 
                      (df['카페인유무'] == caffeine) & 
                      (df['커피유무'] != coffee)]
+    
+    # 예외 처리: 조건에 맞는 음료가 0개일 경우
+    if len(filtered_df) == 0:
+        return jsonify({"error": "조건에 맞는 음료가 없습니다."}), 404
     
     # 칼로리와 당류 특성만 추출하여 스케일링 (표준화)
     scaler = StandardScaler()
