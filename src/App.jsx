780575006ac6a6a4b378127876a406c582e1d866
yeonu2 <amyamy7365@gmail.com>
2024-08-21 17:15:03 +0900

    폰트 변경 및 css 일부 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css';
 import Home from './pages/Home';
 import CafeSelect from './pages/CafeSelect';
 import Part1 from './pages/Part1';
