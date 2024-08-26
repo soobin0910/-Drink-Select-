0c557dfcee83d517baeb099575ee099937690cbc
soobin0910 <kl8501@naver.com>
2024-08-26 18:47:20 +0900

    Update app.py
    필터링 수정(추천,재추천 모두)

--- app.py
+++ app.py
@@ -29,31 +29,21 @@ def recommend():
     max_calories = data['calories']
     max_sugar = data['sugar']
 
-    
-
-    if caffeine == 0:
-        if coffee == 1:   # 카페인 아니오 선택 & 커피 제외하기 yes
-                    filtered_df = df[(df['카페명'].isin(selected_cafes)) & 
-                     (df['카페인유무'] == caffeine) & 
-                     (df['커피유무'] != coffee)]
-        else:   # 카페인 아니오 선택만
+    if caffeine == 0:  # 카페인 아니오 선택
+        if coffee == 1:  # 카페인 아니오 선택 & 커피 제외하기
             filtered_df = df[(df['카페명'].isin(selected_cafes)) & 
-                     (df['카페인유무'] == caffeine)]
-    else:
-    # 카페인 예 선택만
-        filtered_df = df[(df['카페명'].isin(selected_cafes))]
+                             (df['카페인유무'] == caffeine) & 
+                             (df['커피유무'] != coffee)]
+        else:  # 카페인 아니오 선택 & 커피 포함
+            filtered_df = df[(df['카페명'].isin(selected_cafes)) & 
+                             (df['카페인유무'] == caffeine)]
+    else:  # 카페인 예 선택
+        if coffee == 1:  # 카페인 예 선택 & 커피 제외하기
+            filtered_df = df[(df['카페명'].isin(selected_cafes)) & 
+                             (df['커피유무'] != coffee)]
+        else:  # 카페인 예 선택 & 커피 포함
+            filtered_df = df[(df['카페명'].isin(selected_cafes))]
 
-
-    '''
-    if caffeine == 0:
-        df['카페인유무'] == caffeine
-
-    # 선택한 카페 목록과 조건에 맞는 데이터 필터링
-    filtered_df = df[(df['카페명'].isin(selected_cafes)) & 
-                     (df['카페인유무'] == caffeine) & 
-                     (df['커피유무'] != coffee)]
-                     '''
-    
     # 칼로리와 당류 특성만 추출하여 스케일링 (표준화)
     scaler = StandardScaler()
     scaled_features = scaler.fit_transform(filtered_df[['칼로리', '당류']])
@@ -86,11 +76,21 @@ def recommend_more():
     max_calories = data['calories']
     max_sugar = data['sugar']
 
-    # 선택한 카페 목록과 조건에 맞는 데이터 필터링
-    filtered_df = df[(df['카페명'].isin(selected_cafes)) & 
-                     (df['카페인유무'] == caffeine) & 
-                     (df['커피유무'] != coffee)]
-    
+    if caffeine == 0:  # 카페인 아니오 선택
+        if coffee == 1:  # 카페인 아니오 선택 & 커피 제외하기
+            filtered_df = df[(df['카페명'].isin(selected_cafes)) & 
+                             (df['카페인유무'] == caffeine) & 
+                             (df['커피유무'] != coffee)]
+        else:  # 카페인 아니오 선택 & 커피 포함
+            filtered_df = df[(df['카페명'].isin(selected_cafes)) & 
+                             (df['카페인유무'] == caffeine)]
+    else:  # 카페인 예 선택
+        if coffee == 1:  # 카페인 예 선택 & 커피 제외하기
+            filtered_df = df[(df['카페명'].isin(selected_cafes)) & 
+                             (df['커피유무'] != coffee)]
+        else:  # 카페인 예 선택 & 커피 포함
+            filtered_df = df[(df['카페명'].isin(selected_cafes))]
+
     # 칼로리와 당류 특성만 추출하여 스케일링 (표준화)
     scaler = StandardScaler()
     scaled_features = scaler.fit_transform(filtered_df[['칼로리', '당류']])
